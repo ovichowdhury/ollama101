c462f9c82be5094d5d492196dc98a8cb45afd64c
Nahid Chowdhury <nahid@erainfotechbd.com>
2025-01-29 19:42:56 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from ollama import chat
+from flask import Flask, request, send_from_directory
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+import uuid
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app)
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'client.html')
+
+@socketio.on('connect')
+def handle_connect():
+    client_id = request.sid
+    print(f'Client connected: {client_id}')
+
+
+@socketio.on('message')
+def handle_message(data):
+    user_message = data['message']
+    stream = chat(
+        model='deepseek-r1:1.5b',
+        messages=[{'role': 'user', 'content': user_message}],
+        stream=True,
+    )
+    random_id = str(uuid.uuid4())
+    for chunk in stream:
+        emit('response', {'message': chunk['message']['content'], 'id': random_id}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
