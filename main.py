9289bcaa225e9395086aaa6fc8b7aa34c5e64c12
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-03-02 09:46:07 +0600

    feat: start pm2 script and enter feature added

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def handle_message(data):
     user_message = data['message']
     stream = chat(
         # model='deepseek-r1:1.5b',
-        model='llama3.2',
+        model='llama3',
         messages=[{'role': 'user', 'content': user_message}],
         stream=True,
     )
@@ -32,4 +32,4 @@ def handle_message(data):
         emit('response', {'message': chunk['message']['content'], 'id': random_id}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
---
+++ start.sh
@@ -0,0 +1,2 @@
+# Start with pm2
+pm2 start main.py --name llm --interpreter "/home/era/Documents/Python/ollama101/ai-env/bin/python"
